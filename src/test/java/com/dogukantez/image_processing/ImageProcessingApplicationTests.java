6f0c0e180141da7c9a4f22d312e8ed49acc22ae4
Doğukan Tez <dogukantez7@gmail.com>
2026-02-27 12:49:18 +0300

    init

---
+++ src/test/java/com/dogukantez/image_processing/ImageProcessingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dogukantez.image_processing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImageProcessingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
